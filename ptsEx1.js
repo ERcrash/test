2009f1e1dd8e074af7140f1e14336c8c691afa15
Edsson Rodriguez <eddy_2020@hotmail.com>
2020-02-08 12:54:26 -0600

    Esto fue una prueba unicamente

---
+++ ptsEx1.js
@@ -0,0 +1,6 @@
+var nombre;
+var apellido;
+nombre = prompt("Ingrese su nombre: ");
+apellido = prompt("Ingrese su apellido: ")
+console.log("Nombre: " +nombre +" " +apellido );
+document.write("Nombre: " +nombre +" "  +apellido);
